2a8a1a620739c2d4c9414caa2679c5e1a6a75804
Amanp30~ <amanpareek300@gmail.com>
2023-01-06 17:46:59 +0530

    index js

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,14 @@ import Link from "next/link";
 
 function index() {
   return (
-    <div>
-      <Link href="/autoimage">Auto Image</Link>
-      <Link href="/actionsdropdown">Actions Dropdown</Link>
-    </div>
+    <>
+      <div className="divtwoji">
+        <h1>Aasan Components</h1>
+        <a href="https://github.com/Amanp30/aasan-components">Github</a>
+        <Link href="/autoimage">Auto Image</Link>
+        <Link href="/actionsdropdown">Actions Dropdown</Link>
+      </div>
+    </>
   );
 }
 
