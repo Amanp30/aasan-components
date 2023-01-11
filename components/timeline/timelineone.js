2a10893b7d9da3ccffc50fac2d1be234edc53e1f
Amanp30~ <amanpareek300@gmail.com>
2023-01-11 19:13:24 +0530

    timeline added

--- components/timeline/timelineone.js
+++ components/timeline/timelineone.js
@@ -4,19 +4,27 @@ import css from "../../styles/timelineone.module.css";
 function timeline({ data }) {
   return (
     <>
-      <ul className={css.timelineul}>
-        {data?.map((item, index) => {
-          return (
-            <>
-              <li key={index} className={css.timelineli}>
-                <h2>{item.heading}</h2>
-                <span>{item.year}</span>
-                <p>{item.description}</p>
-              </li>
-            </>
-          );
-        })}
-      </ul>
+      <div className={css.timelineone}>
+        <div className={css.timelined}>
+          <div className={css.timelineimgbox}>
+            <img src={data?.[0]?.image} />
+          </div>
+          <h2>{data?.[0]?.title}</h2>
+        </div>
+        <ul className={css.timelineul}>
+          {data?.[1].timelines?.map((item, index) => {
+            return (
+              <>
+                <li key={index} className={css.timelineli}>
+                  <h3>{item.heading}</h3>
+                  <span>{item.year}</span>
+                  <p>{item.description}</p>
+                </li>
+              </>
+            );
+          })}
+        </ul>
+      </div>
     </>
   );
 }
